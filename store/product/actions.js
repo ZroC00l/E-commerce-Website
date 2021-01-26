25e8754de2bf6682ac136fe4a8593e8d837b1923
ZroC00l <giftgabanakgosi@gmail.com>
2021-01-26 09:21:47 +0200

    addcart has been updated to check if a product has been added to a cart

--- store/product/actions.js
+++ store/product/actions.js
@@ -23,7 +23,9 @@ export function productDetails({commit}, id) {
 
 export function addCart({commit, getters}, payload){
     let cart = getters.cart
-    cart.push(payload)
+    let data = payload.product
+    data["quantity"] = payload.quantity
+    cart.push(data)
     commit("setCart", cart)
 }
 
