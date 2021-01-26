3ac684a4a17681bf0df9fddc916eda4239c5647f
ZroC00l <giftgabanakgosi@gmail.com>
2021-01-26 20:47:21 +0200

    update

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -18,14 +18,14 @@
                             <small
                                 id="emailHelp"
                                 class="form-text text-muted"
-                            >We'll never share ypur email with anyone else.</small>
+                            >We'll never share your email with anyone else.</small>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputPassword1">Password</label>
                             <input type="password" class="form-control" id="exampleInputPassword1"/>
                         </div>
                         <div class="form-group form-check">
-                            <input type="checkout" class="form-check-input" id="exampleCheck1" />
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1" />
                             <label class="form-check-label" for="exampleCheck1">Check me out</label>
                         </div>
                         <button @click="login()" type="submit" class="btn btn-primary btn-block">Login</button>
