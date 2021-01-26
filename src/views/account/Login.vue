a657e62eca72107ba74c621db385b2ce973cdc4e
ZroC00l <giftgabanakgosi@gmail.com>
2021-01-26 11:12:41 +0200

    added styling to the login page

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -35,3 +35,26 @@
         </div>
     </div>
 </template>
+<style>
+    .form-control{
+        border-radius: 0%;
+        height: 50px;
+    }
+    .login-form-container{
+        padding: 20px;
+        box-shadow: 0px 2px 5px 2px #888888;
+    }
+    .btn{
+        border-radius: 0%;
+        font-weight: bold;
+        background: teal;
+        border:teal;
+    }
+    .btn:hover{
+        background: #00b4b4;
+    }
+    .btn:focus{
+        background: teal;
+    }
+ 
+</style>
